d5e2d86bcec53bee2b8fe05e29f9d4718e6b5b29
Mario <mario.teik@gmail.com>
2022-11-04 12:21:34 -0300

    add subcategories

--- src/routes/category/index.ts
+++ src/routes/category/index.ts
@@ -2,19 +2,31 @@ import { NowRequestHandler } from "fastify-now";
 import supabase from "../../infrastructure/common/supabase";
 
 type Get = NowRequestHandler<{
-  Reply: { categories: Category[]; count: number } | { message: string };
+  Reply: { categories: Category[] } | { message: string };
 }>;
 
 export const GET: Get = async (req, rep) => {
+  const { data: rootCategory, error: errorRootCategory } = await supabase
+    .from("categories")
+    .select("id, title, type, parent")
+    .is("parent", null)
+    .single();
+
+  if (errorRootCategory) {
+    rep.status(500).send({ message: "No categories found" });
+    return;
+  }
+
   const { data, error } = await supabase
     .from("categories")
-    .select("id, title, type");
+    .select("id, title, type, parent")
+    .eq("parent", rootCategory?.id);
 
   if (error) {
     rep.status(500).send(error);
   }
 
-  return { categories: data ?? [] };
+  return { categories: [rootCategory, ...(data as Category[])] ?? [] };
 };
 
 GET.opts = {};
